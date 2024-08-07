f69a58a0deaeebd8ab64168b55264145dc82a6a9
marina-nabil-anis <marina.nabil910@gmail.com>
2024-08-07 20:02:21 +0300

    marina commit

---
+++ Final_Project.js
@@ -0,0 +1,67 @@
+var FirstBTN =document.getElementById('OverViewBTN')
+var SecondBTN =document.getElementById('InternalStructureBTN')
+var ThirdBTN =document.getElementById('SurfaceGeologyBTN')
+var FirstP =document.getElementById('OverVeiwP')
+var SecondP =document.getElementById('InternalP')
+var ThirdP =document.getElementById('GeologyP')
+var FirstPic =document.getElementById('MainMercuryPic')
+var SecondPic =document.getElementById('InternalMercuryIMG')
+var ThirdPic =document.getElementById('geologypic')
+
+FirstBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('#419EBB')
+    SecondBTN.style.backgroundColor=('Transparent')
+    ThirdBTN.style.backgroundColor=('Transparent')
+    FirstP.style.display=('flex')
+    SecondP.style.display=('none')
+    ThirdP.style.display=('none')
+    FirstPic.style.display=('flex')
+    SecondPic.style.display=('none')
+    ThirdPic.style.display=('none')
+
+}
+SecondBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('Transparent')
+    SecondBTN.style.backgroundColor=('#419EBB')
+    ThirdBTN.style.backgroundColor=('Transparent')
+    FirstP.style.display=('none')
+    SecondP.style.display=('flex')
+    ThirdP.style.display=('none')
+    FirstPic.style.display=('none')
+    SecondPic.style.display=('flex')
+    ThirdPic.style.display=('none')
+
+}
+
+ThirdBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('Transparent')
+    SecondBTN.style.backgroundColor=('Transparent')
+    ThirdBTN.style.backgroundColor=('#419EBB')
+    FirstP.style.display=('none')
+    SecondP.style.display=('none')
+    ThirdP.style.display=('flex')
+    FirstPic.style.display=('flex')
+    SecondPic.style.display=('none')
+    ThirdPic.style.display=('flex')
+
+}
+
+var Menu = document.getElementById("PlanetNames")
+var MobileBTN = document.getElementById('mobilemenu')
+
+MobileBTN.onclick =function(){
+   
+
+    var mybtn = MobileBTN.getAttribute('mybtn')
+
+    if (mybtn == 'true') {
+        Menu.style.display=('flex')
+        MobileBTN.setAttribute('mybtn','false')
+
+
+    }else{
+        Menu.style.display=('none')
+        MobileBTN.setAttribute('mybtn','true')
+
+    }
+  } 
---
+++ neptune.js
@@ -0,0 +1,67 @@
+var FirstBTN =document.getElementById('OverViewBTN')
+var SecondBTN =document.getElementById('InternalStructureBTN')
+var ThirdBTN =document.getElementById('SurfaceGeologyBTN')
+var FirstP =document.getElementById('OverVeiwP')
+var SecondP =document.getElementById('InternalP')
+var ThirdP =document.getElementById('GeologyP')
+var FirstPic =document.getElementById('MainNeptunePic')
+var SecondPic =document.getElementById('InternalNeptuneIMG')
+var ThirdPic =document.getElementById('geologypic')
+
+FirstBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('#2D68F0')
+    SecondBTN.style.backgroundColor=('Transparent')
+    ThirdBTN.style.backgroundColor=('Transparent')
+    FirstP.style.display=('flex')
+    SecondP.style.display=('none')
+    ThirdP.style.display=('none')
+    FirstPic.style.display=('flex')
+    SecondPic.style.display=('none')
+    ThirdPic.style.display=('none')
+
+}
+SecondBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('Transparent')
+    SecondBTN.style.backgroundColor=('#2D68F0')
+    ThirdBTN.style.backgroundColor=('Transparent')
+    FirstP.style.display=('none')
+    SecondP.style.display=('flex')
+    ThirdP.style.display=('none')
+    FirstPic.style.display=('none')
+    SecondPic.style.display=('flex')
+    ThirdPic.style.display=('none')
+
+}
+
+ThirdBTN.onclick=function(){
+    FirstBTN.style.backgroundColor=('Transparent')
+    SecondBTN.style.backgroundColor=('Transparent')
+    ThirdBTN.style.backgroundColor=('#2D68F0')
+    FirstP.style.display=('none')
+    SecondP.style.display=('none')
+    ThirdP.style.display=('flex')
+    FirstPic.style.display=('flex')
+    SecondPic.style.display=('none')
+    ThirdPic.style.display=('flex')
+
+}
+
+var Menu = document.getElementById("PlanetNames")
+var MobileBTN = document.getElementById('mobilemenu')
+
+MobileBTN.onclick =function(){
+   
+
+    var mybtn = MobileBTN.getAttribute('mybtn')
+
+    if (mybtn == 'true') {
+        Menu.style.display=('flex')
+        MobileBTN.setAttribute('mybtn','false')
+
+
+    }else{
+        Menu.style.display=('none')
+        MobileBTN.setAttribute('mybtn','true')
+
+    }
+  } 
